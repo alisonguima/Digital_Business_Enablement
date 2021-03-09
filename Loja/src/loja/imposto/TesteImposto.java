56fe59cf7a004456c1cebf6acbcda31d375e2b25
Alison Guimaraes <alisonguimaraes17@gmail.com>
2021-03-08 23:11:44 -0300

    Design Patterns

---
+++ Loja/src/loja/imposto/TesteImposto.java
@@ -0,0 +1,24 @@
+package loja.imposto;
+
+import java.math.BigDecimal;
+
+import loja.Pedido;
+
+public class TesteImposto {
+
+	public static void main(String[] args) {
+		
+		CalculadoraDeImposto calculadoraDeImposto = new CalculadoraDeImposto(new IPI());
+		Pedido pedido = new Pedido(new BigDecimal("100"));
+		
+		BigDecimal imposto = calculadoraDeImposto.calcular(pedido);
+	
+		System.out.println(imposto);
+		
+		//Trocar a estratégia
+		calculadoraDeImposto.setImposto(new ICMS());
+		imposto = calculadoraDeImposto.calcular(pedido);
+		
+		System.out.println(imposto);
+	}
+}
