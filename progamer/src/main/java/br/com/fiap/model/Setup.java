d3ab9b9edb2258b9e34ff07d2a0cb21fa82840fa
alisonguima <alisonguimaraes17@gmail.com>
2021-04-19 18:50:05 -0300

    Atualizando projeto

---
+++ progamer/src/main/java/br/com/fiap/model/Setup.java
@@ -0,0 +1,66 @@
+package br.com.fiap.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_SETUP")
+public class Setup {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_SETUP")
+	private Long id;
+	
+	@Column(name = "NM_SETUP", nullable = false, length = 30)
+	private String name;
+	
+	@Column(name = "DS_SETUP", nullable = false, length = 255)
+	private String description;
+	
+	@Column(name = "VL_SETUP", nullable = false)
+	private BigDecimal price = new BigDecimal(1000);
+
+	public Setup() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Setup [name=" + name + ", description=" + description + ", price=" + price + "]";
+	}
+	
+	
+}
